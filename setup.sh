7f8847a9fcc3c9ba70b7b8aa84f74b44078903e1
SvetoslavIvanov98 <svetli2012@hotmail.com>
2025-06-22 08:30:16 +0300

    Update setup script: prevent pip from using --user in virtual environment and install LunarVim

--- setup.sh
+++ setup.sh
@@ -92,7 +92,10 @@ python3 -m venv ~/.local/share/lunarvim-venv
 source ~/.local/share/lunarvim-venv/bin/activate
 pip install --upgrade pip
 
-# --- Install LunarVim ---
+# Prevent pip from using --user in venv
+export PIP_USER=0
+
+# Install LunarVim
 LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
 
 deactivate
