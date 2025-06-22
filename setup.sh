4562939eb09ede38149e45abbf6a6b864054536a
SvetoslavIvanov98 <svetli2012@hotmail.com>
2025-06-22 08:56:07 +0300

    Update setup script: set PIP_BREAK_SYSTEM_PACKAGES to avoid conflicts during LunarVim installation

--- setup.sh
+++ setup.sh
@@ -104,6 +104,7 @@ export LV_SKIP_PYTHON_DEPS=1
 export LV_SKIP_NODE_DEPS=1
 
 # --- Install LunarVim using venv Python ---
+export PIP_BREAK_SYSTEM_PACKAGES=1
 PYTHON=$(which python)
 LV_BRANCH='release-1.4/neovim-0.9' PYTHON="$PYTHON" bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
 
